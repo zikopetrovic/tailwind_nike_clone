b14c819f87e7980d79fae9c873251cb086f8c548
Ziko Petrovic <zpetrovic96@outlook.com>
2024-03-10 00:41:12 +0100

    Adding Navbar section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,40 @@
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+
+import { navLinks } from "../constants";
+
 const Nav = () => {
-  return <div>TEST</div>;
+  return (
+    <header className="padding-x absolute py-8 z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((element) => (
+            <li key={element.label}>
+              <a
+                href={element.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {element.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <img
+            src={hamburger}
+            alt="Hamburger"
+            width={25}
+            height={25}
+            className="hidden max-lg:block"
+          />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
