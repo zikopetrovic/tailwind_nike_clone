c9447363942505a51aedb0b2065d9ff7a17a2895
Ziko Petrovic <zpetrovic96@outlook.com>
2024-03-28 22:16:41 +0100

    fix indentation

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -39,7 +39,10 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+      <div
+        className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 
+      bg-primary bg-hero bg-cover bg-center"
+      >
         <img
           src={bigShoe1}
           alt="Big Shoe 1"
